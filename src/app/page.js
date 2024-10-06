ff9f7f80664f68eede9601f146765af578f727d8
Ade Indra Pratama <adeprathama@gmail.com>
2024-10-06 21:53:41 +0700

    Finish create carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,62 @@
-import Image from "next/image";
+"use client";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import BlueBg from "./assets/image/blue-bg.jpg";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <h2>Welcome to Dealls Job</h2>
+      <Carousel className="job-carousel">
+        <div>
+            <img
+                src={BlueBg.src}
+                alt="image1"
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <p className="legend">
+                Image 1
+            </p>
         </div>
-      </main>
+        <div>
+            <img
+                src={BlueBg.src}
+                alt="image2"
+            />
+            <p className="legend">
+                Image 2
+            </p>
+        </div>
+        <div>
+            <img
+                src={BlueBg.src}
+                alt="image3"
+            />
+            <p className="legend">
+                Image 3
+            </p>
+        </div>
+        <div>
+            <img
+                src={BlueBg.src}
+                alt="image4"
+            />
+            <p className="legend">
+                Image 4
+            </p>
+        </div>
+        <div>
+            <img
+                src={BlueBg.src}
+                alt="image5"
+            />
+            <p className="legend">
+                Image 5
+            </p>
+        </div>
+      </Carousel>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <p>Copyright &copy; 2024 by Dealls Job</p>
       </footer>
     </div>
   );
